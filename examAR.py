1324247805597b91a6560dd9d360de6e5dea0bcd
Andrés Rendón <100229140+NRDRND@users.noreply.github.com>
2022-03-15 09:46:04 -0600

    update final
    entrega final

--- examAR.py
+++ examAR.py
@@ -1,7 +1,10 @@
 
 ### Introducir un numero por teclado y decir si es par o impar
 num = int(input('Introduzca un numero: '))
-ifnum % 2 == 0:
-print('Par')
+if num % 2 == 0:
+  print('Par')
 else:
-print('Impar')
+  print('Impar')
+
+print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
+print('José Andrés Rendón Hernández -1522822-')
